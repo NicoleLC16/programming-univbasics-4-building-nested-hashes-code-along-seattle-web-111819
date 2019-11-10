fed111a8c052846fcb5cc71329cd28a191178de9
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-10 00:30:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,15 @@ def third_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
-      :matriarch => {:name => "La Montague", :age => "53"},
-      :hero => {},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15",
+        :status => "alive"
+      },
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lord Montague", :age => "53"},
       :heroine => {},
       :heroine_friends => []
       }
